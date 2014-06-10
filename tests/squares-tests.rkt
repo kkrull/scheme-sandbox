0ea1ed16f3e9b5214db1401a7cdce079b123cf0e
Kyle Krull <kkrull@indigobio.com>
2014-06-10 14:16:28 -0500

    Format

--- tests/squares-tests.rkt
+++ tests/squares-tests.rkt
@@ -4,6 +4,6 @@
   (require rackunit rackunit/text-ui "../src/squares.rkt")
   (run-tests
    (test-suite "square-it"
-               (test-case "given a number"
-                          (check-equal? 1 (square-it 1))
-                          (check-equal? 4 (square-it 2))))))
+     (test-case "given a number"
+       (check-equal? 1 (square-it 1))
+       (check-equal? 4 (square-it 2))))))
